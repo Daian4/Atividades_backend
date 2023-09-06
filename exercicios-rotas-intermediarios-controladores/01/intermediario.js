4be8f5ee8449470064e356ffdb0a8d5f387c4e2e
Daiana Menezes <daianalimaep@gmail.com>
2023-09-06 17:26:16 -0300

    Aprendendo a criar um servidor

--- exercicios-rotas-intermediarios-controladores/01/intermediario.js
+++
@@ -1,6 +0,0 @@
-const validarNumeros = (req, res, next) => {
-    if (!req.query.num1 || !req.query.num2) { return res.send('indique dois números') }
-    next()
-}
-
-module.exports = validarNumeros
--- exercicios-rotas-intermediarios-controladores/02/bancoDeDados.js
+++
@@ -1,4 +0,0 @@
-const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
-
-
-module.exports = jogadores
